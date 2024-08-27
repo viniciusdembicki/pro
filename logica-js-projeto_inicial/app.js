052686b7818ba4c6f95356a2e2e2cf3d48794fa1
viniciusdembicki <116916226+viniciusdembicki@users.noreply.github.com>
2024-08-27 19:52:04 -0300

    asd

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,75 @@
-alert('Boas vindas ao numero secreto');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um número entre 1 e 30');
+while
+(chute != numeroSecreto)
 
-// se chute for igual ao número secreto
-if(chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto (29)');
-
-} else {
-    alert ('Você errou :-(')
-}
+alert('Boas
+  vindas ao numero secreto');
+  let
+  numeroSecreto = 6;
+  
+  
+  console.log(numeroSecreto)
+  
+  
+  let chute
+  
+  
+   
+  
+  
+  //enquanto chute não for igual ao numero Secreto
+  
+  
+  while
+  (chute != numeroSecreto) {
+  
+  
+   
+    chute = prompt('Escolha um número entre 1 e 10');
+  
+  
+   
+    // se chute for igual ao número secreto
+  
+  
+   
+    if (chute == numeroSecreto) {
+  
+  
+   
+        alert(`Isso aí! Você descobriu o número secreto
+  ${numeroSecreto}`);
+  
+  
+   
+  
+  
+   
+    } else {
+  
+  
+   
+        if (chute > numeroSecreto) {
+  
+  
+   
+        alert (`O numeroSecreto é menor que ${chute}`);
+  
+  
+   
+        } else {
+  
+  
+   
+        alert (`O numeroSecreto é maior que ${chute}`);
+  
+  
+   
+        }
+  
+  
+   
+    }
+  
+  
+  }
+  
