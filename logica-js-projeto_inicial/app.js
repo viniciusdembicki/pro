37dcf6885ff3e4efe079fe33665fdd2551b35aa9
viniciusdembicki <116916226+viniciusdembicki@users.noreply.github.com>
2024-08-27 19:44:57 -0300

    asd

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,3 +1,11 @@
-alert('Boas-vindas ao jogo do número secreto'); 
+alert('Boas vindas ao numero secreto');
 let numeroSecreto = 29;
-prompt('Escolha um número entre 1 e 30');
+let chute = prompt('Escolha um número entre 1 e 30');
+
+// se chute for igual ao número secreto
+if(chute == numeroSecreto) {
+    console.log('Isso aí! Você descobriu o número secreto (29)');
+
+} else {
+    alert ('Você errou :-(')
+}
